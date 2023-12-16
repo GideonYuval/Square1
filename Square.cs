5c18e4e48abfde379c2323f405e98837b7cdea94
Gideon Yuval <72211753+GideonYuval@users.noreply.github.com>
2023-12-16 09:44:08 +0200

    Create Square.cs
    added starter code

---
+++ Square.cs
@@ -0,0 +1,12 @@
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static int CalculateSquare(int side)
+        {
+            //your code here;
+        }
+    }
